119de8d44e8103e5dd687bb773ab2b761dcf440e
Maicon Douglas <Mdfilipiaki@gmail.com>
2021-06-02 12:26:42 -0300

    Classes

---
+++ 8-ByteBank/ContaCorrente.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace _8_ByteBank
+{
+    class CurrentAccount
+    {
+        public Client titular;
+        public int agency;
+        public int number;
+        public double balance = 100.00;
+
+        public bool Sacar(double value)
+        {
+            if (value > this.balance)
+            {
+                return false;
+            }
+            this.balance = this.balance - value;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            this.balance += valor;
+        }
+
+        public bool Transferir(double valor, CurrentAccount currentAccount)
+        {
+            if (this.balance < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.balance -= valor;
+                currentAccount.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
---
+++ 8-ByteBank/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_ByteBank
+{
+    class Program
+    {
+        private int GenerateNumberRandom()
+        {
+            Random random = new Random();
+            return random.Next(0, 10000);
+        }
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            client.cpf = "00000000";
+            client.nome = "Maicon Douglas Filipiaki";
+            client.profissao = "Arquiteto";
+            CurrentAccount currentAccount = new CurrentAccount();
+            currentAccount.titular = client;
+
+            Console.WriteLine(currentAccount.titular.nome);
+            //CurrentAccount currentAccount = new CurrentAccount();
+            //currentAccount.titular = "Maicon";
+
+            //CurrentAccount currentAccount2 = new CurrentAccount();
+            //currentAccount2.titular = "Maicon";
+
+            // Console.WriteLine(currentAccount.balance);
+            // Console.WriteLine(currentAccount.ToString());
+            //Console.WriteLine("ANTES DO SAQUE " + currentAccount.balance);
+            //currentAccount.Sacar(150);
+            //Console.WriteLine("DEPOIS DO SAQUE " + currentAccount.balance);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
